c54cd7a1539fce4042fa6505d92744cfbc52a34f
Golik Andrii <Golik.Andrii@gmail.com>
2023-04-12 12:14:44 +0200

    First rows for WORD

---
+++ Main.java
@@ -0,0 +1,682 @@
+public class Main {
+    public static void main(String[] args) {
+        rowFirst();
+        System.out.println();
+        row_2();
+        System.out.println();
+        row_3();
+        System.out.println();
+        row_4();
+        System.out.println();
+        row_5();
+        System.out.println();
+        row_6();
+        System.out.println();
+        row_7();
+        System.out.println();
+        row_8();
+        System.out.println();
+        row_9();
+        System.out.println();
+        row_10();
+        System.out.println();
+        row_11();
+        System.out.println();
+        row_12();
+        System.out.println();
+        row_13();
+        System.out.println();
+        row_14();
+        System.out.println();
+        row_15();
+    }
+
+    public static void rowFirst(){
+        for (int i = 0; i<14; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<11; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<8; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<6; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<8; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<5; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<8; ++i){
+            System.out.print(' ');
+        }
+
+        for (int i = 0; i<5; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_2(){
+        for (int i = 0; i<14; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<11; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<7; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<8; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<7; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<7; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+
+        for (int i = 0; i<7; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_3(){
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<9; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<13; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<3; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<3; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<6; ++i){
+            System.out.print(' ');
+        }
+
+        for (int i = 0; i<7; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<7; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_4(){
+//        T
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+//        E
+        for (int i = 0; i<9; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<12; ++i){
+            System.out.print(' ');
+        }
+        //        A
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+//M
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<1; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<3; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<2; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<3; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<1; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_5(){
+//        T
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+//        E
+        for (int i = 0; i<9; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<11; ++i){
+            System.out.print(' ');
+        }
+        //        A
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<6; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+//M
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<2; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<2; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<2; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<2; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<2; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_6(){
+//        T
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+//        E
+        for (int i = 0; i<9; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<11; ++i){
+            System.out.print(' ');
+        }
+        //        A
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<6; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+//M
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<2; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<6; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<2; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_7(){
+//        T
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+//        E
+        for (int i = 0; i<9; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<11; ++i){
+            System.out.print(' ');
+        }
+        //        A
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<6; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+//M
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<3; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<3; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_8(){
+//        T
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+//        E
+        for (int i = 0; i<9; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<8; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<7; ++i){
+            System.out.print(' ');
+        }
+        //        A
+        for (int i = 0; i<14; ++i){
+            System.out.print('*');
+        }
+
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+//M
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<2; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_9(){
+//        T
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+//        E
+        for (int i = 0; i<9; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<8; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<7; ++i){
+            System.out.print(' ');
+        }
+        //        A
+        for (int i = 0; i<14; ++i){
+            System.out.print('*');
+        }
+
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+//M
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<10; ++i) {
+            System.out.print(' ');
+        }
+
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_10(){
+//        T
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+//        E
+        for (int i = 0; i<9; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<11; ++i){
+            System.out.print(' ');
+        }
+        //        A
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+
+        for (int i = 0; i<6; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+//M
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<10; ++i) {
+            System.out.print(' ');
+        }
+
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_11(){
+//        T
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+//        E
+        for (int i = 0; i<9; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<11; ++i){
+            System.out.print(' ');
+        }
+        //        A
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+
+        for (int i = 0; i<6; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+//M
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<10; ++i) {
+            System.out.print(' ');
+        }
+
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_12(){
+//        T
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+//        E
+        for (int i = 0; i<9; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<11; ++i){
+            System.out.print(' ');
+        }
+        //        A
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+
+        for (int i = 0; i<6; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+//M
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<10; ++i) {
+            System.out.print(' ');
+        }
+
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_13(){
+//        T
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+//        E
+        for (int i = 0; i<9; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<11; ++i){
+            System.out.print(' ');
+        }
+        //        A
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+
+        for (int i = 0; i<6; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+//M
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<10; ++i) {
+            System.out.print(' ');
+        }
+
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_14(){
+//        T
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+//        E
+        for (int i = 0; i<9; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<11; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+        //        A
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+
+        for (int i = 0; i<6; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+//M
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<10; ++i) {
+            System.out.print(' ');
+        }
+
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+    }
+    public static void row_15(){
+//        T
+        for (int i = 0; i<5; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+//        E
+        for (int i = 0; i<9; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<11; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+        //        A
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+
+        for (int i = 0; i<6; ++i){
+            System.out.print(' ');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<4; ++i){
+            System.out.print(' ');
+        }
+//M
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+        for (int i = 0; i<10; ++i) {
+            System.out.print(' ');
+        }
+
+        for (int i = 0; i<4; ++i){
+            System.out.print('*');
+        }
+    }
+}
